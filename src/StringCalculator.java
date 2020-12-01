8d111c4df5fca896f55242491dbeef3ad1bb8e16
Vaishnavi Naidu <vaishnaidu37@gmail.com>
2020-12-01 17:50:54 +0530

    Initial commit

---
+++ src/StringCalculator.java
@@ -0,0 +1,9 @@
+
+public class StringCalculator {
+
+	public Object Add(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/StringCalculatorTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class StringCalculatorTest {
+StringCalculator cal=new StringCalculator();
+
+
+@Test
+public void returnOnEmpty() {
+	assertEquals(0,cal.Add(""));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
